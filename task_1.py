1e95c6383a51cddf5c05794fb11d0a535b8f3052
savio-bijo-thomas <saviobijothomas2028@cs.sjcetpalai.ac.in>
2024-10-15 14:05:52 +0530

    Add files via upload
    I have coded a program to determine whether a given number is a positive or negative integer.

---
+++ task_1.py
@@ -0,0 +1,9 @@
+"""Author:Savio Bijo Thomas
+   Date:15-10-2024
+   purpose:to check whether the given number is +ve or -ve."""
+
+num=int(input("enter a number:"))
+if num >0 :
+    print("the given number",num,"is positive")
+else:
+    print("the given number",num,"is negative")
